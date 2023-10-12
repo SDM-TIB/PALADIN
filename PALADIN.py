02659ea775192820be87ca31f204a16e8df59ae5
Philipp Rohde <Philipp.Rohde@tib.eu>
2023-10-12 09:32:18 +0200

    improve modularization

--- PALADIN.py
+++ PALADIN.py
@@ -6,6 +6,7 @@ PALADIN Tree Validator
 @author: Philipp D. Rohde
 """
 
+import abc
 import argparse
 import json
 from enum import Enum
@@ -14,64 +15,103 @@ from functools import partial
 import mysql.connector
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-
 print_mode = True
 
 
-def generate_db_connection(ip, port, db_name, user, password):
-    db_conn = mysql.connector.connect(
-         host=ip,
-         port=port,
-         user=user,
-         password=password,
-         database=db_name
-       )
-    db_cursor = db_conn.cursor()
-    return {"db_mngr": db_conn, "db_crs": db_cursor}
+class DataSource(object):
+    """Abstract class representing a data source."""
+
+    def __init__(self):
+        self.connection = self._get_connection()
+
+    @staticmethod
+    def get_data_source(data):
+        """Get an appropriate ``DataSource`` instance based on the information in the PALADIN tree."""
+        if data['data_source'] == 'mysql':
+            return MySQL(data)
+        elif data['data_source'] == 'sparql-endpoint':
+            return SPARQLEndpoint(data)
+        else:
+            raise NotImplementedError('Data source type ' + str(data['data_source']) + ' not supported.')
+
+    @abc.abstractmethod
+    def _get_connection(self):
+        """Private method to establish the connection to the data source."""
+        return
+
+    @abc.abstractmethod
+    def query(self, query: str) -> set:
+        """Executes a query over the data source and returns the result as a set."""
+        return set()
 
 
-def sparql_endpoint_connection(url):
-    sparql_endpoint = SPARQLWrapper(url)
-    sparql_endpoint.setReturnFormat(JSON)
-    return sparql_endpoint
+class MySQL(DataSource):
+    """Implementation of a MySQL data source. Handles the connection as well as queries to the data."""
+
+    def __init__(self, data):
+        self.url = data['url']
+        self.port = data['port']
+        self.database = data['database']
+        self.user = data['user']
+        self.password = data['password']
+        super().__init__()
+
+    def __del__(self):
+        """Close the connection to the database when the object is deleted."""
+        self.close_connection()
+
+    def _get_connection(self):
+        db_conn = mysql.connector.connect(
+            host=self.url,
+            port=self.port,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+        db_cursor = db_conn.cursor()
+        return {"db_mngr": db_conn, "db_crs": db_cursor}
+
+    def close_connection(self):
+        """Closes the MySQL connection."""
+        self.connection['db_crs'].close()
+
+    def query(self, query: str) -> set:
+        try:
+            db_crs = self.connection['db_crs']
+            db_crs.execute(query)
+            return set([x[0] for x in db_crs.fetchall()])
+        except mysql.connector.Error:
+            print('Error in the SQL query')
+        return set()
 
 
-def close_connection(db_mngr):
-    db_mngr["db_crs"].close()
+class SPARQLEndpoint(DataSource):
+    """Implementation of a SPARQL endpoint. Handles the connection as well as queries to the data."""
+
+    def __init__(self, data):
+        self.url = data['url']
+        super().__init__()
+
+    def _get_connection(self):
+        sparql_endpoint = SPARQLWrapper(self.url)
+        sparql_endpoint.setReturnFormat(JSON)
+        return sparql_endpoint
+
+    def query(self, query: str) -> set:
+        try:
+            self.connection.setQuery(query)
+            result = self.connection.queryAndConvert()
+            proj_var = result['head']['vars'][0]
+            return set([res[proj_var]['value'] for res in result['results']['bindings']])
+        except:
+            print('Error in the SPARQL query')
+        return set()
 
 
-def query_data_mysql(query, conn):
-    try:
-        conn["db_crs"].execute(query)
-        data = set([x[0] for x in conn["db_crs"].fetchall()])
-        return data
-    except mysql.connector.Error:
-        print('Error in the SQL query')
-    return set()
-
-
-def query_data_sparql(query, conn):
-    try:
-        conn.setQuery(query)
-        result = conn.queryAndConvert()
-        proj_var = result['head']['vars'][0]
-        return set([res[proj_var]['value'] for res in result['results']['bindings']])
-    except:
-        print('Error in the SPARQL query')
-    return set()
-
-
-def query_data(query, data_source, conn):
-    if data_source == 'mysql':
-        return query_data_mysql(query, conn)
-    elif data_source == 'sparql-endpoint':
-        return query_data_sparql(query, conn)
-
-
-def traverse_tree_recursion(tree, population, data_source, conn):
+def traverse_tree_recursion(tree, population, data_source: DataSource):
     """Traverse the tree in a recursive fashion."""
     if tree['target']:
-        target = query_data(tree['target'], data_source, conn)
+        target = data_source.query(tree['target'])
         population = population.intersection(target)
     
     validated = population
@@ -81,7 +121,7 @@ def traverse_tree_recursion(tree, population, data_source, conn):
         print('Target population: ' + str(len(population)))
     
     if tree['validation']:
-        validated = query_data(tree['validation'], data_source, conn)
+        validated = data_source.query(tree['validation'])
         validated = validated.intersection(population)
         unvalidated = population - validated
         if print_mode:
@@ -92,12 +132,12 @@ def traverse_tree_recursion(tree, population, data_source, conn):
         print('---------------------------')
     
     if tree['left'] and len(unvalidated) > 0:
-        traverse_tree_recursion(tree['left'], validated, data_source, conn)
+        traverse_tree_recursion(tree['left'], validated, data_source)
     if tree['right'] and len(validated) > 0:
-        traverse_tree_recursion(tree['right'], unvalidated, data_source, conn)
+        traverse_tree_recursion(tree['right'], unvalidated, data_source)
     
 
-def traverse_tree_depth(tree, population, data_source, conn):
+def traverse_tree_depth(tree, population, data_source: DataSource):
     """Traverse the tree with Depth-first search (DFS)."""
     stack = []
     stack.append((tree, population,))
@@ -106,7 +146,7 @@ def traverse_tree_depth(tree, population, data_source, conn):
         tree, population = stack.pop()
 
         if tree['target']:
-            target = query_data(tree['target'], data_source, conn)
+            target = data_source.query(tree['target'])
             population = population.intersection(target)
 
         validated = population
@@ -116,7 +156,7 @@ def traverse_tree_depth(tree, population, data_source, conn):
             print('Target population: ' + str(len(population)))
 
         if tree['validation']:
-            validated = query_data(tree['validation'], data_source, conn)
+            validated = data_source.query(tree['validation'])
             validated = validated.intersection(population)
             unvalidated = population - validated
             if print_mode:
@@ -132,7 +172,7 @@ def traverse_tree_depth(tree, population, data_source, conn):
             stack.append((tree['left'], validated,))
         
 
-def traverse_tree_width(tree, population, data_source, conn):
+def traverse_tree_width(tree, population, data_source: DataSource):
     """Traverse the tree with Breadth-first search (BFS)."""
     stack_level = []
     stack_next_level = []
@@ -147,7 +187,7 @@ def traverse_tree_width(tree, population, data_source, conn):
             tree, population = stack_level.pop()
 
             if tree['target']:
-                target = query_data(tree['target'], data_source, conn)
+                target = data_source.query(tree['target'])
                 population = population.intersection(target)
 
             validated = population
@@ -157,7 +197,7 @@ def traverse_tree_width(tree, population, data_source, conn):
                 print('Target population: ' + str(len(population)))
             
             if tree['validation']:
-                validated = query_data(tree['validation'], data_source, conn)
+                validated = data_source.query(tree['validation'])
                 validated = validated.intersection(population)
                 unvalidated = population - validated
                 if print_mode:
@@ -179,9 +219,9 @@ class TraversalStrategy(Enum):
     DFS = partial(traverse_tree_depth)
     REC = partial(traverse_tree_recursion)
 
-    def traverse(self, tree, population, data_source, conn):
+    def traverse(self, tree, population, data_source):
         """Calls the traversal function mapped to this instance of the enum."""
-        return self.value(tree, population, data_source, conn)
+        return self.value(tree, population, data_source)
 
 
 def paladin(tree_file, traversal_strategy):
@@ -189,17 +229,10 @@ def paladin(tree_file, traversal_strategy):
     traversal_strategy = TraversalStrategy[traversal_strategy]
     with open(tree_file, 'r') as tf:
         data = json.load(tf)
-
-    if data['data_source'] == 'mysql':
-        db_mngr = generate_db_connection(data['url'], data['port'], data['database'], data['user'], data['password'])
-        db_mngr["db_crs"].execute(data['population'])
-        population = set([x[0] for x in db_mngr["db_crs"].fetchall()])
-        traversal_strategy.traverse(data['tree'], population, data['data_source'], db_mngr)
-        close_connection(db_mngr)
-    elif data['data_source'] == 'sparql-endpoint':
-        endpoint = sparql_endpoint_connection(data['url'])
-        population = query_data_sparql(data['population'], endpoint)
-        traversal_strategy.traverse(data['tree'], population, data['data_source'], endpoint)
+    data_source = DataSource.get_data_source(data)
+    population = data_source.query(data['population'])
+    traversal_strategy.traverse(data['tree'], population, data_source)
+    del data_source
 
 
 if __name__ == '__main__':
